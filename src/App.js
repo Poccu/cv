68c253e3c7329f27b004677d3e352a13fe74f456
Poccu <mmordoboy@mail.ru>
2022-02-11 05:59:49 +0300

    Add Dark/Light theme toggle button

--- src/App.js
+++ src/App.js
@@ -2,23 +2,72 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import About from './pages/About'
+import Weather from './pages/Weather'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import { IconButton, Box } from '@mui/material'
+
+const themeLight = createTheme({
+  palette: {
+    background: {
+      default: '#eeeeee',
+    },
+  },
+})
+
+const themeDark = createTheme({
+  palette: {
+    background: {
+      default: '#191919',
+    },
+    text: {
+      primary: '#ffffff',
+    },
+  },
+})
+
+const style = {
+  position: 'absolute',
+  // paddingTop: '15px',
+  bottom: '47%',
+  left: 0,
+}
 
 export default function App() {
+  const [light, setLight] = React.useState(false)
+
   return (
-    <Router>
-      <Header />
-      <main>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <ThemeProvider theme={light ? themeLight : themeDark}>
+      <CssBaseline />
+      <Router>
+        <Header />
+        <main>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/weather" element={<Weather />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+      <Box style={style}>
+        <IconButton
+          onClick={() => setLight((prev) => !prev)}
+          color="inherit"
+          size="large"
+        >
+          {light ? (
+            <DarkModeOutlinedIcon fontSize="inherit" />
+          ) : (
+            <LightModeOutlinedIcon fontSize="inherit" />
+          )}
+        </IconButton>
+      </Box>
+    </ThemeProvider>
   )
 }
