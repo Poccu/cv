da99bf42b17ba16c08d1f990f2fe41f4fdf55c54
Poccu <mmordoboy@mail.ru>
2022-02-21 15:36:47 +0300

    Add Dropdown menu on phone

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState } from 'react'
 import {
   AppBar,
   Container,
@@ -18,6 +18,54 @@ import MenuIcon from '@mui/icons-material/Menu'
 import NightsStayOutlinedIcon from '@mui/icons-material/NightsStayOutlined'
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange'
 import WarningAmberIcon from '@mui/icons-material/WarningAmber'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemText from '@mui/material/ListItemText'
+import { Link as RouterLink } from 'react-router-dom'
+import Divider from '@mui/material/Divider'
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: 6,
+    // border: '1px solid',
+    // borderColor: theme.palette.divider,
+    marginTop: theme.spacing(3),
+    minWidth: 180,
+    color: theme.palette.text.primary,
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.35) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}))
 
 const ThemeButton = styled(Button)(({ theme }) => ({
   fontSize: 16,
@@ -37,72 +85,118 @@ const ThemeButton = styled(Button)(({ theme }) => ({
 }))
 
 export default function Header() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <header>
       <AppBar
-        // style={{ backgroundColor: '#212121' }}
         position="fixed"
-        // color="transparent"
         color="primary"
         elevation={0}
         sx={{ boxShadow: 7, display: { xs: 'block', md: 'none' } }}
-        // enableColorOnDark
       >
         <Container maxwidth="sm">
           <Toolbar>
-            {/* <NavLink to="/">
-              <IconButton edge="start" to="/">
-                <Avatar
-                  alt="Avatar"
-                  src="/logo7.png"
-                  sx={{ width: 50, height: 50 }}
-                />
-              </IconButton>
-            </NavLink> */}
             <Box sx={{ flexGrow: 1 }}>
-              <IconButton edge="start" color="inherit" title="Menu">
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+              >
                 <MenuIcon />
               </IconButton>
-              {/* style={{ fontSize: 30 }} */}
-              {/* <Stack direction="row" spacing={1}>
-                <NavLink
-                  to="/"
-                  style={{ textDecoration: 'none', color: '#fff' }}
+              <StyledMenu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  'aria-labelledby': 'basic-button',
+                }}
+              >
+                <MenuItem component={RouterLink} to="/" onClick={handleClose}>
+                  <CottageOutlinedIcon />
+                  <ListItemText>Home</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem
+                  component={RouterLink}
+                  to="/weather"
+                  onClick={handleClose}
                 >
-                  <ThemeButton
-                    startIcon={<CottageOutlinedIcon style={{ fontSize: 24 }} />}
-                  >
-                    Home
-                  </ThemeButton>
-                </NavLink>
-                <NavLink to="/weather" style={{ textDecoration: 'none' }}>
-                  <ThemeButton
-                    startIcon={<WbSunnyOutlinedIcon style={{ fontSize: 24 }} />}
-                  >
-                    Weather
-                  </ThemeButton>
-                </NavLink>
-                <NavLink to="/dictionary" style={{ textDecoration: 'none' }}>
-                  <ThemeButton
-                    startIcon={<TranslateIcon style={{ fontSize: 24 }} />}
-                  >
-                    Dictionary
-                  </ThemeButton>
-                </NavLink>
-              </Stack> */}
+                  <NightsStayOutlinedIcon />
+                  <ListItemText>Weather</ListItemText>
+                </MenuItem>
+                <MenuItem
+                  component={RouterLink}
+                  to="/dictionary"
+                  onClick={handleClose}
+                >
+                  <TranslateIcon />
+                  <ListItemText>Dictionary</ListItemText>
+                </MenuItem>
+                <MenuItem
+                  component={RouterLink}
+                  to="/exchange"
+                  onClick={handleClose}
+                >
+                  <CurrencyExchangeIcon />
+                  <ListItemText>Exchange</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem
+                  component={RouterLink}
+                  to="/404"
+                  onClick={handleClose}
+                >
+                  <WarningAmberIcon />
+                  <ListItemText>404</ListItemText>
+                </MenuItem>
+                {/* <MenuItem
+                  component={RouterLink}
+                  to="/cut"
+                  onClick={handleClose}
+                >
+                  <CurrencyExchangeIcon />
+                  <ListItemText>Cut</ListItemText>
+                  <Typography variant="inherit" color="text.secondary">
+                    ⌘X
+                  </Typography>
+                </MenuItem> */}
+              </StyledMenu>
             </Box>
-            <Socials />
+            <Box sx={{ mr: -1 }}>
+              <Socials />
+              {/* <NavLink to="/">
+                <IconButton edge="start" to="/">
+                  <Avatar
+                    alt="Avatar"
+                    src="/logo7.png"
+                    sx={{ width: 50, height: 50 }}
+                  />
+                </IconButton>
+              </NavLink> */}
+            </Box>
           </Toolbar>
         </Container>
       </AppBar>
       <AppBar
-        // style={{ backgroundColor: '#212121' }}
         position="fixed"
-        // color="transparent"
         color="primary"
         elevation={0}
         sx={{ boxShadow: 7, display: { xs: 'none', md: 'block' } }}
-        // enableColorOnDark
       >
         <Container maxwidth="sm">
           <Toolbar>
